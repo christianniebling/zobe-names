7f7d616d4041143dfdaad7c2388e989a941367c9
Christian Niebling <christian.v.niebling@gmail.com>
2021-01-06 12:12:50 -0500

    branches

--- zobe.py
+++ zobe.py
@@ -7,10 +7,10 @@
 ## if full names were made up of 4 names... 36^4 = 1679616 names
 import random
 
-shuffle_switch = 0 #1=shuffle; 0=no_shuffle
+shuffle_switch = 1 #1=shuffle; 0=no_shuffle
 
 file1 = open("output.txt","w+")
-col_count = 15
+col_count = 10 
 
 words = ["bran","ch","es","ca"]
 names=[]
@@ -21,20 +21,12 @@ for entry in words:
             for entry4 in words:
                 names.append(entry+entry2+entry3+entry4)
 
-print(names)
 
-##list1 = []
-##for entry in names:
-##    for entry2 in names:
-##        list1.append(entry+" "+entry2)
-####        for entry3 in names:
-####            file1.write(entry+" "+entry2+" "+entry3+"; ")
-##
-##if shuffle_switch:    
-##    random.shuffle(list1)
-##for idx, listitem in enumerate(list1):
-##    file1.write('%s, ' % listitem)
-##    if(idx+1)%col_count == 0:
-##        file1.write('\n')
+if shuffle_switch:    
+    random.shuffle(names)
+for idx, listitem in enumerate(names):
+    file1.write('%s, ' % listitem)
+    if(idx+1)%col_count == 0:
+        file1.write('\n')
 file1.close()
 
